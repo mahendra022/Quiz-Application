307b4bd7b281f0c5f713627d7b749dc4f9caa66b
Mahendra022 <ekamahendra86@gmail.com>
2021-08-03 01:41:36 +0700

    Create routing user select level question

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/screen/quiz_page.dart';
+import 'package:quiz/screen/quiz_page_easy.dart';
+import 'package:quiz/screen/quiz_page_medium.dart';
+import 'package:quiz/screen/quiz_page_hard.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  void _onLoading() {
+  void _onLoadingLevel1() {
     showDialog(
       context: context,
       barrierDismissible: false,
@@ -25,10 +27,60 @@ class _HomePageState extends State<HomePage> {
         );
       },
     );
-    new Future.delayed(new Duration(seconds: 2), () {
+    new Future.delayed(new Duration(seconds: 1), () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => QuizPage()),
+        MaterialPageRoute(builder: (context) => QuizPageEasy()),
+      );
+    });
+  }
+
+  void _onLoadingLevel2() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              new CircularProgressIndicator(
+                  backgroundColor: Colors.transparent),
+              new Text("Loading Stage"),
+            ],
+          ),
+        );
+      },
+    );
+    new Future.delayed(new Duration(seconds: 1), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => QuizPageMedium()),
+      );
+    });
+  }
+
+  void _onLoadingLevel3() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              new CircularProgressIndicator(
+                  backgroundColor: Colors.transparent),
+              new Text("Loading Stage"),
+            ],
+          ),
+        );
+      },
+    );
+    new Future.delayed(new Duration(seconds: 1), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => QuizPageHard()),
       );
     });
   }
@@ -41,7 +93,7 @@ class _HomePageState extends State<HomePage> {
           margin: const EdgeInsets.only(top: 20),
           child: FlatButton.icon(
             onPressed: () {
-              _onLoading();
+              _showModalDialog(context);
             },
             label: Text(
               "Play",
@@ -62,7 +114,7 @@ class _HomePageState extends State<HomePage> {
               _showModalDialog(context);
             },
             label: Text(
-              "Select level",
+              "Setting",
               style: TextStyle(color: Colors.black, fontFamily: "Sen"),
             ),
             icon: Icon(
@@ -84,7 +136,7 @@ class _HomePageState extends State<HomePage> {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
               child: Container(
-                constraints: BoxConstraints(maxHeight: 170, maxWidth: 40),
+                constraints: BoxConstraints(maxHeight: 200, maxWidth: 40),
                 child: Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: Column(
@@ -92,10 +144,18 @@ class _HomePageState extends State<HomePage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Center(
+                          child: Text("Select your Level",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: "Sen",
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                        Center(
                             child: Container(
                           child: FlatButton(
                             onPressed: () {
-                              _onLoading();
+                              _onLoadingLevel1();
                             },
                             child: Text(
                               "Easy",
@@ -109,7 +169,7 @@ class _HomePageState extends State<HomePage> {
                             child: Container(
                           child: FlatButton(
                             onPressed: () {
-                              _onLoading();
+                              _onLoadingLevel2();
                             },
                             child: Text(
                               "Medium",
@@ -123,7 +183,7 @@ class _HomePageState extends State<HomePage> {
                             child: Container(
                           child: FlatButton(
                             onPressed: () {
-                              _onLoading();
+                              _onLoadingLevel3();
                             },
                             child: Text(
                               "Hard",
@@ -146,7 +206,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "IQ-Quiz",
+              "Math-Quiz",
               style: TextStyle(
                   color: Colors.blue,
                   fontSize: 60,
